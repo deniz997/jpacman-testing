ef290e601c699905ec4b08fdbc47b03084066344
Deniz Mert <deniz.mert.tecimer@gmail.com>
2021-04-30 16:07:30 +0200

    BoardTest: BoardTest file created and testBoardCreation added

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,23 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoardTest {
+
+    /**
+     * Tests if board is created correctly by checking its dimensions and its content
+     */
+    @Test
+    void testBoardCreation() {
+        BasicSquare basicSquare = new BasicSquare();
+        Board tBoard = new Board(new Square[][]{new Square[]{basicSquare}});
+        assertThat(tBoard.getHeight()).isEqualTo(1);
+        assertThat(tBoard.getWidth()).isEqualTo(1);
+        assertThat(tBoard.invariant()).isEqualTo(true);
+        assertThat(tBoard.squareAt(0,0)).isEqualTo(basicSquare);
+    }
+}
