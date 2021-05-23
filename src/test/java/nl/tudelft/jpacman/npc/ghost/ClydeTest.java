f364dfbf401405ba36934f43a408926dd640520a
Günther Wullaert <gunther.wullaert@hotmail.com>
2021-05-23 08:35:11 +0200

    ClydeTest,Tools: Added a test if clyde moves to the player if shyness away and tools to create tests easier

--- src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/ClydeTest.java
@@ -1,2 +1,64 @@
-package nl.tudelft.jpacman.npc.ghost;public class ClydeTest {
+package nl.tudelft.jpacman.npc.ghost;
+
+import nl.tudelft.jpacman.board.BoardFactory;
+import nl.tudelft.jpacman.board.Direction;
+import nl.tudelft.jpacman.level.Level;
+import nl.tudelft.jpacman.level.LevelFactory;
+import nl.tudelft.jpacman.level.Player;
+import nl.tudelft.jpacman.level.PlayerFactory;
+import nl.tudelft.jpacman.points.DefaultPointCalculator;
+import nl.tudelft.jpacman.points.PointCalculator;
+import nl.tudelft.jpacman.sprite.PacManSprites;
+import nl.tudelft.jpacman.tools.Tools;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test if the ghost Clyde is behaving correctly.
+ */
+public class ClydeTest {
+    private GhostMapParser ghostMapParser;
+    private Player player;
+
+    /**
+     * Initializes the GhostMapParser for every test.
+     */
+    @BeforeEach
+    public void initTests() {
+        PacManSprites pacManSprites = new PacManSprites();
+        GhostFactory ghostFactory = new GhostFactory(pacManSprites);
+        PointCalculator pointCalculator = new DefaultPointCalculator();
+        LevelFactory levelFactory = new LevelFactory(pacManSprites, ghostFactory, pointCalculator);
+        BoardFactory boardFactory = new BoardFactory(pacManSprites);
+        PlayerFactory playerFactory = new PlayerFactory(pacManSprites);
+        player = playerFactory.createPacMan();
+        ghostMapParser = new GhostMapParser(levelFactory, boardFactory, ghostFactory);
+    }
+
+    /**
+     * Tests if Clyde moves closer to the player
+     * if he is more than shyness(default=8) spaces away from player.
+     */
+    @Test
+    public void testIfClydeMovesToPlayerIfMoreThanShynessSpacesAway() {
+        char[][] map = new char[][]{
+            "############".toCharArray(),
+            "#P        C#".toCharArray(),
+            "############".toCharArray()
+        };
+        Level level = ghostMapParser.parseMap(Tools.rotateMap(map));
+
+        level.registerPlayer(player);
+        player.setDirection(Direction.EAST);
+
+        Clyde clyde = Navigation.findUnitInBoard(Clyde.class, level.getBoard());
+        assertThat(clyde).isNotNull();
+        Optional<Direction> dir = clyde.nextAiMove();
+        assertThat(dir).isPresent();
+        assertThat(dir.get()).isEqualTo(Direction.WEST);
+    }
 }
